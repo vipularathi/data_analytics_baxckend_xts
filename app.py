a83f9a6493222547bd4875c187050f085116cfb2
Abhishek Pahwa <apahwa014@gmail.com>
2023-12-11 16:58:47 +0530

    update: endpoints

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from datetime import date
 import numpy as np
 import pandas as pd
 import uvicorn
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, status
+from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 from contracts import get_req_contracts
 from db_ops import DBHandler
@@ -22,9 +24,16 @@ class ServiceApp:
         self.symbol_expiry_map = None
 
     def add_routes(self):
+        self.app.add_api_route('/', methods=['GET'], endpoint=self.default)
         self.app.add_api_route('/symbol', methods=['GET'], endpoint=self.get_symbols)
         self.app.add_api_route('/straddle/minima', methods=['GET'], endpoint=self.fetch_straddle_minima)
         self.app.add_api_route('/straddle/iv', methods=['GET'], endpoint=self.fetch_straddle_iv)
+        self.app.add_api_route('/straddle/cluster', methods=['GET'], endpoint=self.fetch_straddle_cluster)
+
+    @staticmethod
+    def default():
+        body = {'status': 'success', 'message': '', 'data': [], 'response_code': None}
+        return JSONResponse(content=jsonable_encoder(body), status_code=status.HTTP_200_OK)
 
     def get_symbols(self):
         if self.symbol_expiry_map is None:
@@ -43,11 +52,25 @@ class ServiceApp:
         df = DBHandler.get_straddle_iv_data(symbol, expiry)
         return self._straddle_response(df)
 
-    def _straddle_response(self, df: pd.DataFrame):
+    def fetch_straddle_cluster(self, symbol: str = Query(), expiry: date = Query(), interval: str = Query('5min')):
+        df = DBHandler.get_straddle_iv_data(symbol, expiry)
+        allowed = pd.date_range(df['ts'].min(), df['ts'].max(), freq=interval)
+        req = df[df['ts'].isin(allowed)].copy()
+        req = self._straddle_response(req, raw=True)
+        req = req.replace({np.NAN: None}).round(2)
+        strike_iv = req.groupby(['strike'], as_index=False).agg({'combined_iv': list})
+        strike_iv.sort_values(['strike'], inplace=True)
+        strikes = strike_iv['strike'].tolist()
+        iv = list(zip(*strike_iv['combined_iv'].tolist()))
+        return {'strikes': strikes, 'iv': iv}
+
+    def _straddle_response(self, df: pd.DataFrame, raw=False):
         df['range'] = (df['spot'] - df['strike']).abs() < (df['spot'] * 0.05)
         df = df[df['range']].copy()
         df.drop(columns=['spot', 'range'], errors='ignore', inplace=True)
         df.sort_values(['ts', 'strike'], inplace=True)
+        if raw:
+            return df
         return self.df_response(df, to_millis=['ts'])
 
     @staticmethod
